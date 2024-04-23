7928cd2b104832011e212450b4b14fe165c1f738
prince35raj <prince35.raj@gmail.com>
2024-04-23 10:59:21 +0530

    SMS services implmented

--- src/main/java/com/RWI/Nidhi/user/controller/UserController.java
+++ src/main/java/com/RWI/Nidhi/user/controller/UserController.java
@@ -5,29 +5,33 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping("/user")
 public class UserController {
 
-    @Autowired
-    UserServiceImplementation userService;
+	@Autowired
+	UserServiceImplementation userService;
 
-    @PostMapping("/sendEmailOtp")
-    public ResponseEntity<String> sendEmailOtp(String email) throws Exception {
-        return userService.sendEmailOtp(email);
-    }
-    @PostMapping("/verifyEmailOtp")
-    public ResponseEntity<String> verifyEmailOtp(String email, String sentOtp, String enteredOtp) throws Exception {
-        return userService.verifyEmailOtp(email, sentOtp, enteredOtp);
-    }
-    @PostMapping("/sendPhoneOtp")
-    public ResponseEntity<String> sendPhoneOtp(String email) throws Exception {
-        return userService.sendPhoneOtp(email);
-    }
-    @PostMapping("verifyPhoneOtp")
-    public ResponseEntity<String> verifyPhoneOtp(String email, String sentOtp, String enteredOtp) throws Exception {
-        return userService.verifyPhoneOtp(email, sentOtp, enteredOtp);
-    }
+	@PostMapping("/sendEmailOtp")
+	public ResponseEntity<String> sendEmailOtp(String email) throws Exception {
+		return userService.sendEmailOtp(email);
+	}
+
+	@PostMapping("/verifyEmailOtp")
+	public ResponseEntity<String> verifyEmailOtp(String email, String sentOtp, String enteredOtp) throws Exception {
+		return userService.verifyEmailOtp(email, sentOtp, enteredOtp);
+	}
+
+	@PostMapping("/sendPhoneOtp")
+	public ResponseEntity<String> sendPhoneOtp(@RequestParam("phoneNumber") String phoneNumber) throws Exception {
+		return userService.sendPhoneOtp(phoneNumber);
+	}
+
+	@PostMapping("/verifyPhoneOtp")
+	public ResponseEntity<String> verifyPhoneOtp(@RequestParam("phoneNumber") String phoneNumber, @RequestParam("receivedOtp") String enteredOtp) throws Exception {
+		return userService.verifyPhoneOtp(phoneNumber, enteredOtp);
+	}
 }
--- src/main/java/com/RWI/Nidhi/user/serviceInterface/UserServiceInterface.java
+++ src/main/java/com/RWI/Nidhi/user/serviceInterface/UserServiceInterface.java
@@ -7,5 +7,5 @@ public interface UserServiceInterface {
     ResponseEntity<String> sendEmailOtp(String email) throws Exception;
     ResponseEntity<String> verifyEmailOtp(String email, String sentOtp, String enteredOtp) throws Exception;
     ResponseEntity<String> sendPhoneOtp(String phoneNumber) throws Exception;
-    ResponseEntity<String> verifyPhoneOtp(String phoneNumber, String sentOtp, String enteredOtp) throws Exception;
+    ResponseEntity<String> verifyPhoneOtp(String phoneNumber, String enteredOtp) throws Exception;
 }
