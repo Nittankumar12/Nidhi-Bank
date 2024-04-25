e3cfad6e6d81f70e57c25accd76e240d6c7bd7a5
nittan <nitin01kumar14@gmail.com>
2024-04-25 13:00:43 +0530

    All controllers working

--- src/main/java/com/RWI/Nidhi/user/serviceImplementation/AccountsServiceImplementation.java
+++ src/main/java/com/RWI/Nidhi/user/serviceImplementation/AccountsServiceImplementation.java
@@ -9,10 +9,13 @@ import com.RWI.Nidhi.user.repository.AccountsRepo;
 import com.RWI.Nidhi.user.serviceInterface.AccountsService;
 import com.RWI.Nidhi.user.serviceInterface.SchemeService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
 import java.util.List;
 import java.util.Optional;
 import java.util.Random;
 
+@Service
 public class AccountsServiceImplementation implements AccountsService {
     @Autowired
     AccountsRepo accountsRepo;
