fea8b79a2b8d0795c16b90450f43e9101b043c44
AjeetTanwar <ajeet.tanwar95@gmail.com>
2024-05-07 16:03:51 +0530

    fd and rd acc to email

--- src/main/java/com/RWI/Nidhi/repository/AdminRepo.java
+++ src/main/java/com/RWI/Nidhi/repository/AdminRepo.java
@@ -9,7 +9,7 @@ import org.springframework.stereotype.Repository;
 import java.util.Optional;
 
 @Repository
-
-public interface AdminRepo extends JpaRepository<Admin, Integer> {
+public interface AdminRepo extends JpaRepository<Admin,Integer> {
+    Admin findByAdminName(String adminName);
 
 }
