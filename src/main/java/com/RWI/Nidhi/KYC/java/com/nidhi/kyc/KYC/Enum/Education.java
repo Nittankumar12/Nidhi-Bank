34f6f7f0fb9ed3ef25d5c9c4ae76c8eea50c2d96
aman <domnicaman911@gmail.com>
2024-05-07 12:50:39 +0530

    user kyc module and further config

---
+++ src/main/java/com/RWI/Nidhi/KYC/java/com/nidhi/kyc/KYC/Enum/Education.java
@@ -0,0 +1,10 @@
+package com.nidhi.kyc.KYC.Enum;
+
+public enum Education {
+
+    GRADUATE,
+    POST_GRADUATE,
+    METRIC,
+    INTER,
+    OTHER;
+}
---
+++ src/main/java/com/RWI/Nidhi/KYC/java/com/nidhi/kyc/KYC/KycApplication.java
@@ -0,0 +1,13 @@
+package com.nidhi.kyc.KYC;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KycApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KycApplication.class, args);
+	}
+
+}
