41dd8d3df7f3f101e133abc141f93075607b0598
aman <88793938+Amantastic@users.noreply.github.com>
2024-05-07 12:32:07 +0530

    Revert " added kyc module for user and further config"

--- src/main/java/com/RWI/Nidhi/user/KYC/src/main/java/com/nidhi/kyc/KYC/Enum/Gender.java
+++
@@ -1,8 +0,0 @@
-package com.nidhi.kyc.KYC.Enum;
-
-public enum Gender {
-    MALE,
-    FEMALE,
-    NON_BINARY,
-    OTHER;
-}
--- src/main/java/com/RWI/Nidhi/user/KYC/src/main/java/com/nidhi/kyc/KYC/Repo/AddressRepo.java
+++
@@ -1,7 +0,0 @@
-package com.nidhi.kyc.KYC.Repo;
-
-import com.nidhi.kyc.KYC.Entity.AddressProof;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface AddressRepo extends JpaRepository<AddressProof,Long> {
-}
