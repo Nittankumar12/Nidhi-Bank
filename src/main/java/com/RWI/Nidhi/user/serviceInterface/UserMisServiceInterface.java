ebeaf9901b277b7466f101aadc4c322be302d9f5
AjeetTanwar <ajeet.tanwar95@gmail.com>
2024-05-08 16:02:05 +0530

    fd,rd and mis

--- src/main/java/com/RWI/Nidhi/user/serviceInterface/UserMisServiceInterface.java
+++ src/main/java/com/RWI/Nidhi/user/serviceInterface/UserMisServiceInterface.java
@@ -1,12 +1,13 @@
 package com.RWI.Nidhi.user.serviceInterface;
 
 import com.RWI.Nidhi.dto.MisDto;
+import com.RWI.Nidhi.dto.MisRequestDto;
 import com.RWI.Nidhi.entity.MIS;
 
 
 public interface UserMisServiceInterface {
 
-    MIS createMis(MisDto misDto);
+    MisRequestDto createMis(String agentEmail, String email,MisDto misDto);
 
     Double closeMis(int misId) throws Exception;
 
